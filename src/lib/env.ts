c3e5e8398b34c8827071ef22d0b73d42fd9d1f6b
jsh901220 <joe1220@daum.net>
2019-07-19 22:58:52 +0900

    [bug: type] - [fix] build type 실패(Cannot read property 'LOG_LEVEL' of undefined)

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,5 @@
 import yenv from 'yenv'
-import { logger } from './logger'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-export const env = yenv('env.yaml', {
-  logBeforeThrow: message => logger.error(message)
-})
+export const env = yenv('env.yaml')
--- src/lib/models.ts
+++ src/lib/models.ts
@@ -3,20 +3,13 @@ import { Sequelize } from 'sequelize-typescript'
 import { env } from './env'
 
 const sequelize =  new Sequelize({
-  operatorsAliases: Sequelize.Op as any,
   database: env.DB_DATABASE,
   host: env.DB_HOST,
   dialect: env.DB_DIARECT,
   username: env.DB_USERNAME,
   password: '' + env.DB_PASSWORD,
   port: env.DB_PORT,
-  modelPaths: [path.join(__dirname, '../sequelize/models')],
-  define: {
-    timestamps: true,
-    paranoid: true,
-    charset: 'utf8',
-    dialectOptions: { collate: 'utf8_general_ci' }
-  }
+  modelPaths: [path.join(__dirname, '../sequelize/models')]
 })
 
 export default sequelize
